527bcf8ef02aa3573cad1d380b038484bf1f859a
stepan.kuznetsov <stepan.kuznetsov@id.thesoul.io>
2025-06-17 13:13:49 -0300

    fixes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const headers = document.querySelectorAll('.accordion__header');
 document.querySelectorAll('.accordion__header__wrapper').forEach(wrapper => {
     wrapper.addEventListener('click', () => {
         const item = wrapper.closest('.accordion__item');
         item.classList.toggle('active');
+        document.querySelectorAll('.accordion__item').forEach(i => {
+            if (i !== item) i.classList.remove('active');
+        });
     });
 });
